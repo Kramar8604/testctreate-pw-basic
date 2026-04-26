16a1a43f20ec9b76ba911102011170b1b3046eff
Kramar8604 <kat.gorbach@gmail.com>
2026-04-26 11:59:34 +0200

    Unit-10 update

--- playwright.config.ts
+++ playwright.config.ts
@@ -41,6 +41,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
+    baseURL: 'https://practicesoftwaretesting.com',
     testIdAttribute: 'data-test',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from "@playwright/test";
 
 test('Verify login as a user with valid credentials', async ({ page }) => {
-  await page.goto('https://practicesoftwaretesting.com/auth/login');
-  await page.fill('[data-test="email"]', 'customer@practicesoftwaretesting.com');
-  await page.fill('[data-test="password"]', 'welcome01');
-  await page.click('[data-test="login-submit"]');
-  await expect(page).toHaveURL('https://practicesoftwaretesting.com/account');
+  await page.goto('/auth/login');
+  await page.getByTestId('email').fill('customer@practicesoftwaretesting.com');
+  await page.getByTestId('password').fill('welcome01');
+  await page.getByTestId('login-submit').click();
+  await expect(page).toHaveURL('/account');
   await expect(page.getByTestId('page-title')).toHaveText('My account');
   await expect(page.locator('.navbar')).toContainText('Jane Doe');
 });
